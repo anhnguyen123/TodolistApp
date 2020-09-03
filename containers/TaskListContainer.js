d5bf0d13b9872215ef6438fd14872555fb97a2f9
anhnguyen123 <anhnguyen29989@gmail.com>
2020-09-03 16:40:44 +0700

    add code

--- containers/TaskListContainer.js
+++ containers/TaskListContainer.js
@@ -1,11 +1,11 @@
 import TaskListComponent from '../components/TaskListComponent';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 
-const mapStateToProps = state => {
-    // alert(`state send to task list = ${JSON.stringify(state)}`);
-    return {
-        tasks:state.taskReducers
-    }
+const mapStateToProps = (state) => {
+  // alert(`state send to task list = ${JSON.stringify(state)}`);
+  return {
+    tasks: !state.taskReducers ? [] : state.taskReducers,
+  };
 };
 const TaskListContainer = connect(mapStateToProps)(TaskListComponent);
 export default TaskListContainer;
